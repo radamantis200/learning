f47f27cf934688764cbf9f8cda61ec5679faa845
Jefry <sanchezmorajeffrey@yahoo.com>
2018-08-26 15:43:45 -0600

    controladores

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Student;
+use App\User;
+use App\UserSocialAccount;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\DB;
+
+
+
+
+
 
 class LoginController extends Controller
 {
@@ -17,7 +27,7 @@ class LoginController extends Controller
     | redirecting them to your home screen. The controller uses a trait
     | to conveniently provide its functionality to your applications.
     |
-    */
+     */
 
     use AuthenticatesUsers;
 
@@ -38,16 +48,53 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function redirectToProvider(string $driver){
+    public function redirectToProvider(string $driver)
+    {
         return Socialite::driver($driver)->redirect();
     }
 
-    public function handleProviderCallback(string $driver){
+    public function handleProviderCallback(string $driver)
+    {
         if (!request()->has('code') || request()->has('denied')) {
-            session()->flash('message',['danger',__('Inicio de sesión cancelado')]);
+            session()->flash('message', ['danger', __('Inicio de sesión cancelado')]);
             return redirect('login');
         }
         $socialUser = Socialite::driver($driver)->user();
-        dd($socialUser);
+        $user = null;
+        $success = true;
+        $email = $socialUser->email;
+        $check = DB::table('users')->where('email', '=', $email)->first();
+        // $check = User::whereEmail($email)->first();
+        if ($check) {
+            $user = $check;
+        } else {
+            DB::beginTransaction();
+            try {
+                $user = User::create([
+                    "name" => $socialUser->name,
+                    "email" => $email
+                ]);
+                UserSocialAccount::create([
+                    "user_id" => $user->id,
+                    "provider" => $driver,
+                    "provider_uid" => $socialUser->id
+                ]);
+
+                Student::create([
+                    "user_id" => $user->id,
+                ]);
+
+            } catch (\Exception $exception) {
+                $success = $exception->getMessage();
+                DB::rollBack();
+            }
+        }
+        if ($success === true) {
+            DB::commit();
+            auth()->loginUsingId($user->id);
+            return redirect(route('home'));
+        }
+        session()->flash('message', ['danger', $success]);
+        return redirect('login');
     }
 }
--- app/UserSocialAccount.php
+++ app/UserSocialAccount.php
@@ -4,6 +4,20 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\UserSocialAccount
+ *
+ * @property-read \App\User $user
+ * @mixin \Eloquent
+ * @property int $id
+ * @property int $user_id
+ * @property string $provider
+ * @property string $provider_uid
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserSocialAccount whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserSocialAccount whereProvider($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserSocialAccount whereProviderUid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserSocialAccount whereUserId($value)
+ */
 class UserSocialAccount extends Model
 {
     public function user(){
