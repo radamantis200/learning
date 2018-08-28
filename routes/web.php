0ea70ede18cc3fe4c78e9a3f802d35d9d6364ea8
Jefry <sanchezmorajeffrey@yahoo.com>
2018-08-27 22:19:57 -0600

    actualización

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/set_language/{lang}','Controller@setLanguage')->name('set_language');
 
 Route::get('login/{driver}', 'Auth\LoginController@redirectToProvider')->name('social_auth');
 Route::get('login/{driver}/callback', 'Auth\LoginController@handleProviderCallback');
