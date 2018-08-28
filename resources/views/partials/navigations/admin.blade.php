de178a4e0d985ffa9463118f4d6a61ce6286eddb
Jefry <sanchezmorajeffrey@yahoo.com>
2018-08-27 22:18:41 -0600

    vistas de navegación

---
+++ resources/views/partials/navigations/admin.blade.php
@@ -0,0 +1,5 @@
+<li><a class="nav-link" href="#">{{__("Administrar cursos")}} </a></li>
+<li><a class="nav-link" href="#">{{__("Administrar estudiantes")}} </a></li>
+<li><a class="nav-link" href="#">{{__("Administrar profesores")}} </a></li>
+
+@include('partials.navigations.logged')
---
+++ resources/views/partials/navigations/student.blade.php
@@ -0,0 +1,5 @@
+<li><a class="nav-link" href="#">{{ __("Mi perfil") }}</a></li>
+<li><a class="nav-link" href="#">{{ __("Mis suscripciones") }}</a></li>
+<li><a class="nav-link" href="#">{{ __("Mis facturas") }}</a></li>
+<li><a class="nav-link" href="#">{{ __("Mis cursos") }}</a></li>
+@include('partials.navigations.logged')
